c92aee0a03290cde29a434bf1f76bf1505f9dbf1
l-HeXin <jinjihao113795@outlook.com>
2022-10-24 08:56:01 +0800

    test

---
+++ src/main/java/com/He__Xin/hello_world/Main.java
@@ -0,0 +1,26 @@
+package com.He__Xin.hello_world;
+
+
+import net.minecraft.Util;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("hello_world")
+@Mod.EventBusSubscriber
+public class Main {
+    
+    @SubscribeEvent
+    public static void playerJoinWorld(PlayerEvent.PlayerLoggedInEvent event){
+
+        Player player = event.getPlayer();
+        Level level = player.level;
+
+        player.sendMessage(new TextComponent("Hello "+player.getDisplayName().getString()+".From"+(level.isClientSide()?"CLIENT":"SERVER")+"."),Util.NIL_UUID);
+
+    }
+
+}
